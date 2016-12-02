0094a43bf3bbc2ccbdfdaacf8aa4a90cda09d3fe
Eason Mao <mhaihua@internal.synopsys.com>
2016-12-02 19:17:24 +0800

    update for multithread

--- rsh_ruuner.py
+++ rsh_ruuner.py
@@ -1,4 +1,9 @@
 
+from logging import getLogger, INFO
+from cloghandler import ConcurrentRotatingFileHandler
+import multiprocessing,subprocess
+import os,time
+
 class RSH_RUNNER:
 	__runner_requestor__        =    ''
 	__operating_machine__       =    ''
@@ -7,6 +12,7 @@ class RSH_RUNNER:
 	__parellel_proc__           =    0
 	__command_queue__           =    []
 	__log_id__                  =    ''
+	__return_content__          =    ''
 	
 
 	self.__init__(self,requestor,machine,log_path,parellel_proc,command_queue):
@@ -18,28 +24,51 @@ class RSH_RUNNER:
 
 	def __run__(self):
 		"""
-		func run is the wrapper for the real run
+		func __run__ is the wrapper for the real run
 		"""
-		from logging import getLogger, INFO
-		from cloghandler import ConcurrentRotatingFileHandler
-		import multiprocessing,subprocess
-		import os,time
+		pidlet  = []
+		lock    = multiprocessing.Lock()
+		for cmd in self.__command_queue__:
+			process = multiprocessing.Process(target=__cmd__,args=(lock,cmd))
+			process.start()
+			pidlet.append(process)
 		
+		for p in pidlet:
+			p.join()
+		
+		return self.__return_content__
+
+
+	def __cmd__(self,lock,command):
+
 		log = getLogger()
+
 		# buid log path
-		sefl.__log_id__    = self.__runner_requestor__+'_'+time.time()+'.log'
-		logfile = os.path.join(log_path,self.__log_id__)
+		sefl.__log_id__    =  self.__runner_requestor__+'_'+time.strftime("%Y_%H_%M_%S")+'.log'
+		logfile            =  os.path.join(log_path,self.__log_id__)
 
 		# Rotate log after reaching 10M, keep 5 old copies.
 		rotateHandler = ConcurrentRotatingFileHandler(logfile, "a", 1024*1024*10, 5)
 		log.addHandler(rotateHandler)
 
 
-
-
-		log.setLevel(INFO)
-		log.setLevel(error)
+		p=subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE) 
+		(stdout,errout)    =  p.communicate()
+		if stdout:
+			lock.acquire()
+			self.__return_content__    =+  command + ':' + stdout
+			lock.release()
+			log.setLevel(INFO)
+			log.info(time.strftime("%b %d %Y %H:%M:%S:  ")+command+':'+stdout)
+		if errout:
+			lock.acquire()
+			self.__return_content__    =+  command + ':' + errout
+			lock.release()
+			log.setLevel(ERROR)
+			log.info(time.strftime("%b %d %Y %H:%M:%S:  ")+command+':'+errout)
 		
-		log.info("Here is a very exciting log message, just for you")
+
+
+
 
 
